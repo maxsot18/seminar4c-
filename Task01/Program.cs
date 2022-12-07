cc3701f96c14f4d88d660f324405e6be7a2f70a4
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-07 08:58:52 -0800

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Напишите цикл, который 
+// принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B. Реализовать через функции.
+
+int Prompt(string message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = Prompt("Enter number A");
+int B = Prompt("Enter number B ");
+
+int Degreeofb(int num)
+{
+   
+   
+   int deg = A;
+   for(int i = 1; i < B; i++)
+   {
+    deg = deg * A;
+   }
+   return deg;
+}
+
+
+
+
+int degryofB = Degreeofb (A);
+Console.Write(degryofB); 
