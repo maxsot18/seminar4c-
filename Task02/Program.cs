8488cc4658fc69ba46aa588c8826eece5187c5c5
Maxsot <sotnikov.maxim3007@gmail.com>
2022-12-08 07:15:09 -0800

    Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: 
+// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе. Реализовать через функции.
+int Prompt(string message)
+{
+    Console.WriteLine($"{message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int num = Prompt("Enter a number");
+
+int Sum(int Number)
+{
+    
+    
+    int sumofdig = 0;
+    while(num % 10 > 0)
+    {
+        sumofdig = sumofdig + num %10;
+        num = num / 10;
+        
+        
+    }
+    return sumofdig;
+    
+}
+
+int sum = Sum(num);
+Console.WriteLine($"Sum of digit = {sum}");
